07d140327d43a32cba221c7c2e05e50483fa81e6
Prashant <prashantco111@gmail.com>
2024-01-23 12:29:11 +0530

    Add Dappazone contract and Hardhat configuration

---
+++ contracts/dappazone.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.22;
+
+contract Dappazone {
+  
+}
---
+++ hardhat.config.ts
@@ -0,0 +1,6 @@
+import('@nomicfoundation/hardhat-toolbox')
+
+module.exports = {
+	solidity: '^0.8.22',
+	// ...
+}
